91d80c18be3ba6143704eebd59e1b7dc207651ee
Jakub Walczak <walczak600@gmail.com>
2019-08-04 14:50:54 +0200

    initial structure of application

---
+++ src/main/java/pl/sdacademy/javapoz11programowanie1/books/BooksApplication.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.javapoz11programowanie1.books;
+
+public class BooksApplication {
+    public static void main(String[] args) {
+        new BooksStart().start();
+    }
+}
---
+++ src/main/java/pl/sdacademy/javapoz11programowanie1/books/BooksStart.java
@@ -0,0 +1,40 @@
+package pl.sdacademy.javapoz11programowanie1.books;
+
+import java.util.Scanner;
+
+public class BooksStart {
+    private BooksViews views;
+
+    public BooksStart() {
+        this.views = new BooksViews(new Scanner(System.in));
+    }
+
+    public void start() {
+        boolean flag = true;
+        do {
+            int decision = views.startMenu();
+            switch (decision) {
+                case 1:
+                    authorsView();
+                    break;
+                case 2:
+                    booksView();
+                    break;
+                default:
+                    flag = false;
+
+            }
+        } while (flag);
+
+
+    }
+
+    private void booksView() {
+        System.out.println("Tutaj beą książki");
+    }
+
+    private void authorsView() {
+        System.out.println("Tutaj bea autorzy");
+    }
+
+}
