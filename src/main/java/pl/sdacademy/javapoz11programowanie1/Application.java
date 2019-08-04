88260b3e4c6fecffdf7ee371177a15f7b84cd1a1
Jakub Walczak <walczak600@gmail.com>
2019-08-04 10:01:02 +0200

    some change

--- src/main/java/pl/sdacademy/javapoz11programowanie1/Application.java
+++ src/main/java/pl/sdacademy/javapoz11programowanie1/Application.java
@@ -3,5 +3,6 @@ package pl.sdacademy.javapoz11programowanie1;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("How are You?");
     }
 }
