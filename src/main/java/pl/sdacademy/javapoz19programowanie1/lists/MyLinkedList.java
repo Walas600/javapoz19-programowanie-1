c5ff59dace218e59ea7c2ce12b28043966c32cb1
Jakub Walczak <walczak600@gmail.com>
2019-08-25 15:34:28 +0200

    linked list methods todo

--- src/main/java/pl/sdacademy/javapoz19programowanie1/lists/MyLinkedList.java
+++ src/main/java/pl/sdacademy/javapoz19programowanie1/lists/MyLinkedList.java
@@ -22,6 +22,16 @@ public class MyLinkedList {
         size++;
     }
 
+    //TODO - obsluga pustej listy
+    //TODO - obsliga blednego indexu (ArrayIndexOutOfBoundException)
+    public Integer get(int index){
+        MyLinkedListItem tmpItem = head;
+        for (int i = 0; i < index ; i++) {
+            tmpItem = tmpItem.nextItem;
+        }
+        return tmpItem.value;
+    }
+
     public int size(){
         return size;
     }
--- src/test/java/pl/sdacademy/javapoz19programowanie1/lists/MyLinkedListTest.java
+++ src/test/java/pl/sdacademy/javapoz19programowanie1/lists/MyLinkedListTest.java
@@ -3,6 +3,8 @@ package pl.sdacademy.javapoz19programowanie1.lists;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 public class MyLinkedListTest {
@@ -33,6 +35,33 @@ public class MyLinkedListTest {
 
         //then
         Assert.assertEquals(5,myLinkedList.size());
+
+    }
+
+    @Test
+    public void getShouldReturnFirstElement(){
+        //todo -dopisac testy
+    }
+    @Test
+    public void getShouldReturnLastElement(){
+        //todo - dopisac testy
+    }
+
+    @Test
+    public void getShouldReturnMiddleElement(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+        myLinkedList.add(20);
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+
+        //when
+        Integer result = myLinkedList.get(3);
+
+        //then
+        Assert.assertEquals((Integer)30,result);
 
     }
 }
