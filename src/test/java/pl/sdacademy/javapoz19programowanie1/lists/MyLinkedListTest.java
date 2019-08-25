b86f61a64403335064eda4fb4778717121202498
Jakub Walczak <walczak600@gmail.com>
2019-08-25 15:18:28 +0200

    linked list methods

---
+++ src/test/java/pl/sdacademy/javapoz19programowanie1/lists/MyLinkedListTest.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.javapoz19programowanie1.lists;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyLinkedListTest {
+
+    @Test
+    public void addShouldAddElementToEmptyList(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        //when
+        myLinkedList.add(5);
+
+        //then
+        Assert.assertEquals(1,myLinkedList.size());
+    }
+
+    @Test
+    public void addShouldAddElementToNonEmptyList(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+
+        //when
+        myLinkedList.add(5);
+
+        //then
+        Assert.assertEquals(5,myLinkedList.size());
+
+    }
+}
